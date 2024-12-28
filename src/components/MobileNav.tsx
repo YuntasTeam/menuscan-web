65ef59cde1dfb90ffb27cc5b985f6705b11490e5
TamVanum <gaston.cath14@gmail.com>
2024-12-28 17:29:05 -0300

    feat: simple front con v0

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+
+interface MobileNavProps {
+  sidebarNavItems: Array<{
+    title: string;
+    href: string;
+    icon: string;
+  }>;
+}
+
+const MobileNav: React.FC<MobileNavProps> = ({ sidebarNavItems }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="h-5 w-5"
+          >
+            <path d="M3 3v18h18" />
+            <path d="M18.7 8l-5.1 5.2-2.8-2.7L7 14.3" />
+          </svg>
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="pr-0">
+        <a href="/" className="flex items-center space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="h-6 w-6"
+          >
+            <path d="M3 3v18h18" />
+            <path d="M18.7 8l-5.1 5.2-2.8-2.7L7 14.3" />
+          </svg>
+          <span className="font-bold">MenuScan</span>
+        </a>
+        <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+          <nav className="grid gap-1">
+            {sidebarNavItems.map((item) => (
+              <a
+                key={item.href}
+                href={item.href}
+                className={cn(
+                  "flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+                //   window.location.pathname === item.href
+                //     ? "bg-accent"
+                //     : "transparent"
+                )}
+              >
+                <span>{item.title}</span>
+              </a>
+            ))}
+          </nav>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
